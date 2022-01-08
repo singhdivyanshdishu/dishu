de11897bdb35b34d6a59ca5eb71800eef5a0676d
Divyansh Kumar Singh <75778161+singhdivyanshdishu@users.noreply.github.com>
2022-01-08 16:24:11 +0530

    Create audio.py

---
+++ audio.py
@@ -0,0 +1,11 @@
+import pyttsx3
+import PyPDF2
+book = open('BHMAECC-II.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+page = pdfReader.getPage(7)
+text = page.extractText()
+speaker.say(text)
+speaker.runAndWait()
